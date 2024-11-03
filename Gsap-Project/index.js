b20578f7d313aba7dfe24088767d26caaf8b9abf
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-03 18:05:22 +0530

    gsap project

---
+++ Gsap-Project/index.js
@@ -0,0 +1,34 @@
+// gsap.to("#box", {
+//   rotation: 360,
+//   x: 500,
+//   duration: 1,
+//   backgroundColor: "yellow",
+// });
+
+// gsap from
+
+gsap.from('#box',{
+    rotation:360,
+    x:500,
+    duration:1,
+    backgroundColor:"green"
+})
+
+gsap.from('#box-1',{
+    rotation:360,
+    x:500,
+    duration:3,
+    delay:2,
+    backgroundColor:"green"
+
+})
+
+
+gsap.from('#box-2',{
+    rotation:360,
+    x:500,
+    duration:2,
+     
+    backgroundColor:"green"
+})
+
