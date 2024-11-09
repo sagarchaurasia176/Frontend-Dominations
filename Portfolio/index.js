3aac1d51c2b94e9222241d978194ccfe81d5313b
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-09 20:02:35 +0530

    locomotive again not work with the gsap

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,10 +1,42 @@
+gsap.registerPlugin(ScrollTrigger);
 
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
 
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed",
+});
 
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-var tl = gsap.timeline();
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
 // time line applied there so we get !
-
+var tl = gsap.timeline();
 tl.from("#nav-bar .logo", {
   y: -50,
   opacity: 0,
@@ -45,7 +77,7 @@ gsap.from(".cards .content-of-card", {
   rotate: 0, // Rotate the element by 15 degrees (adjust as needed)
   scrollTrigger: {
     trigger: ".cards",
-    scroller: "body",
+    scroller: "#main",
     start: "top 0",
     end: "top -50%", // Adjust the end position as needed
     scrub: true,
@@ -60,13 +92,10 @@ gsap.to(".text-with-the-scroller .text", {
   // Scroll Trigger applied there so we get
   scrollTrigger: {
     trigger: ".text-with-the-scroller",
-    scroller: "body",
+    scroller: "#main",
     start: "top 0",
     end: "top -100%",
     scrub: 2,
     pin: true,
   },
 });
-
-
-
