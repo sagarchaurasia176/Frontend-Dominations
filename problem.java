1695e09be056738358d370e02ddd8ec0f60bf0d0
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-19 17:09:02 +0530

    working on obys loader

---
+++ problem.java
@@ -0,0 +1,22 @@
+public class problem {
+    public static void main(String[] args) {
+        // non-overlapping intervals
+        // problem => remove overlapping only if its lies in the corner then you can
+        // just ignore it
+        // intervals = [[1,2],[2,3],[3,4],[1,3]]
+        // identified the min
+        int[][] arr = { { 1, 2 }, { 2, 3 }, { 0, 1 } };
+        int cnt = 0;
+        int prev = 0;
+        
+        // for(int i=0; i<=arr.length-1l i++){
+        //     // if(arr[prev] >= ){
+    
+        //     // }
+        // }
+
+
+
+    }
+
+}
